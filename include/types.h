389c7bfa08719ecf011416e11241aa7ebe0f1cbe
Theodore Bradley <gaminggekoman@gmail.com>
2024-11-13 15:31:52 +0000

    Added GLM and some framework for instanced rendering -Added glm headers and include to main -Added types.h to contain engine types -Added Object class which uses the glm primitive types -Added DrawableObject and various supporting types and variables DrawableObject contains the code to draw an object in an instanced fashion. This is achieved by the use of glDrawElementsInstanced and use of an array of element attributes on the main thread. This array will be passed once to the shader, and again if it is changed. Then the renderer will draw an instance of the plane for every sprite on screen. This is achieved by having sprites get an index from a stack for a free spot in the attribute array, which it writes its attributes to, then when they go offscreen, they release that index back to the stack. The onscreen detection, actual rendering, and index management still need to be written.

---
+++ include/types.h
@@ -0,0 +1,97 @@
+#pragma once
+#include <stack>
+
+#define MAX_SPRITES 128
+
+class Object
+{
+public:
+	glm::fvec2 position;
+	glm::float32 rotation;
+	glm::fvec2 scale;
+
+	Object(glm::fvec2 _position, glm::float32 _rotation, glm::fvec2 _scale)
+	{
+		position = _position;
+		rotation = _rotation;
+		scale = _scale;
+	}
+	Object(glm::fvec2 _position, glm::float32 _rotation)
+	{
+		position = _position;
+		rotation = _rotation;
+		scale = glm::fvec2(1.0f, 1.0f);
+	}
+	Object(glm::fvec2 _position)
+	{
+		position = _position;
+		rotation = 0;
+		scale = glm::fvec2(1.0f, 1.0f);
+	}
+	Object(glm::fvec2 _position, glm::fvec2 _scale)
+	{
+		position = _position;
+		rotation = 0;
+		scale = _scale;
+	}
+
+	Object()
+	{
+		position = glm::fvec2(0.0f, 0.0f);
+		rotation = 0;
+		scale = glm::fvec2(1.0f, 1.0f);
+	}
+};
+struct InstanceAttributes
+{
+	glm::fvec2 position;
+	glm::float32 rotation;
+	glm::fvec2 scale;
+	glm::fvec4 atlasRect;
+};
+class DrawableObject : public Object
+{
+private:
+	InstanceAttributes myInstanceAttributes;
+	InstanceAttributes* GlobalInstanceAttributes;
+	unsigned int attributeIndex; //index in the global instanceAttributes array
+
+public:
+
+	DrawableObject(glm::fvec2 _position, glm::fvec4 _atlasRect, InstanceAttributes* _instanceAttributes, std::stack<unsigned int>* freeIndices) : Object(_position)
+	{
+		init(_atlasRect, _instanceAttributes, freeIndices);
+	}
+
+	DrawableObject(glm::fvec2 _position, glm::fvec2 _scale, glm::fvec4 _atlasRect, InstanceAttributes* _instanceAttributes, std::stack<unsigned int>* freeIndices) : Object(_position, _scale)
+	{
+		init(_atlasRect, _instanceAttributes, freeIndices);
+	}
+
+	DrawableObject(glm::fvec2 _position, glm::int32 _rotation, glm::fvec2 _scale, glm::fvec4 _atlasRect, InstanceAttributes* _instanceAttributes, std::stack<unsigned int>* freeIndices) : Object(_position, _rotation, _scale)
+	{
+		init(_atlasRect, _instanceAttributes, freeIndices);
+	}
+
+	void SetAttributes(InstanceAttributes* newAttributes)
+	{
+		GlobalInstanceAttributes[attributeIndex] = *newAttributes;
+	}
+
+	InstanceAttributes* GetAttributes()
+	{
+		return &(GlobalInstanceAttributes[attributeIndex]);
+	}
+
+	void init(glm::fvec4 _atlasRect, InstanceAttributes* _instanceAttributes, std::stack<unsigned int>* freeIndices)
+	{
+		attributeIndex = freeIndices->top(); //get an available index
+		freeIndices->pop(); //remove it from stack as it is no longer free
+		myInstanceAttributes.position = position; //assign attributes to local struct
+		myInstanceAttributes.rotation = rotation; //..
+		myInstanceAttributes.scale = scale; //..
+		myInstanceAttributes.atlasRect = _atlasRect; //..
+		GlobalInstanceAttributes = _instanceAttributes; //copy pointer to global array of attributes
+		SetAttributes(&myInstanceAttributes); //set local copy of attributes to appropriate spot in global attribs
+	}
+};
--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,13 @@
 #include <glad/glad.h>
 #include <SDL/SDL_opengl.h>
 #include <box2d/box2d.h>
-
+#include <GLM/glm.hpp>
+#include "types.h"
 float plane[12] = {
-	-0.5, -0.5, 0.0,
-	0.5, -0.5, 0.0,
-	0.5, 0.5, 0.0,
-	-0.5, 0.5, 0.0};
+	-0.5f, -0.5f, 0.0f,
+	0.5f, -0.5f, 0.0f,
+	0.5f, 0.5f, 0.0f,
+	-0.5f, 0.5f, 0.0f};
 
 unsigned int planeIndices[6] = {
 	0, 1, 2,
@@ -34,53 +35,44 @@ int init();
 static void handleEvents();
 void handleKeys(SDL_KeyboardEvent* key);
 void loop();
+void draw();
+static unsigned int GetNewInstancedAttributeIndex();
 
 SDL_Window* window; //main window
 static SDL_Renderer* renderer; //main renderer
 SDL_GLContext glContext;
 bool running = true;
+GLuint errShader = 0;
+std::stack<unsigned int> FreeInstancedAttributeIndices; //only one copy allowed, main thread access only
+InstanceAttributes GlobalInstancedAttributes[MAX_SPRITES];
+unsigned int onscreenSprites = 0; //number of sprites to draw with glDrawElementsInstanced
 
 int main(int argv, char** args)
 {
 	init();
 	//SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_INFORMATION, "Working", "It's Working!", NULL); //post a basic messagebox
-
 	GLuint VBO = 0;
 	GLuint VIO = 0;
 	GLuint VAO = 0;
-	GLuint defaultProg = 0;
-	GLuint defaultVert = 0;
-	GLuint defaultFrag = 0;
-	glGenBuffers(1, &VBO);
-	glGenBuffers(1, &VIO);
-	glGenVertexArrays(1, &VAO);
-	glBindVertexArray(VAO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(plane), plane, GL_STATIC_DRAW);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, VIO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(planeIndices), planeIndices, GL_STATIC_DRAW);
-
-	defaultVert = glCreateShader(GL_VERTEX_SHADER);
-	defaultFrag = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(defaultVert, 1, &vert_default, NULL);
-	glCompileShader(defaultVert);
-	glShaderSource(defaultFrag, 1, &frag_default, NULL);
-	glCompileShader(defaultFrag);
-
-	defaultProg = glCreateProgram();
-	glAttachShader(defaultProg, defaultVert);
-	glAttachShader(defaultProg, defaultFrag);
-	glLinkProgram(defaultProg);
+	glGenBuffers(1, &VBO); //generate empty buffer
+	glGenBuffers(1, &VIO); //..
+	glGenVertexArrays(1, &VAO); //.. vertex array object
+	glBindVertexArray(VAO); //bind Vertex Array Object
+	glBindBuffer(GL_ARRAY_BUFFER, VBO); //bind buffer
+	glBufferData(GL_ARRAY_BUFFER, sizeof(plane), plane, GL_STATIC_DRAW); //populate buffer
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0); //set attribute read params
+	glEnableVertexAttribArray(0); //enable (read?) attrib
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, VIO); //bind index buffer
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(planeIndices), planeIndices, GL_STATIC_DRAW); //populate
 
 	while (running)
 	{
 		loop();
 		//temp render code
-		glBindVertexArray(VAO);
-		glUseProgram(defaultProg);
-		glDrawElements(GL_TRIANGLES, sizeof(planeIndices), GL_UNSIGNED_INT, 0);
+		glBindVertexArray(VAO); //bind plane VAO
+		glUseProgram(errShader); //use basic shader
+		glDrawElements(GL_TRIANGLES, sizeof(planeIndices), GL_UNSIGNED_INT, 0); //draw indexed verts
+		draw();
 	}
 
 	return 0;
@@ -96,20 +88,20 @@ int init()
 {
 	//Initialization:
 	//SDL:
-	//SDL OpenGL:
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 5);
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+	//SDL with OpenGL:
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4); //use OpenGL 4.5 core
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 5); //..
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE); //..
 	SDL_Init(SDL_INIT_EVERYTHING); //initalize all SDL subsystems
 	window = SDL_CreateWindow("Platformer", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 0, 0, SDL_WINDOW_FULLSCREEN_DESKTOP | SDL_WINDOW_OPENGL); //create new SDL OGL window
 	if (!window) //if failed to create window
 		quit(-1); //quit with error code -1
 	glContext = SDL_GL_CreateContext(window); //Create GL context
-	if (!glContext)
-		quit(-1);
+	if (!glContext) //if failed to create context
+		quit(-1); //quit
 	gladLoadGLLoader(SDL_GL_GetProcAddress); //load GL function pointers through SDL
 	//OpenGL
-	glClearColor(0.53, 0.81, 0.92, 1.0); //set sky blue clear color RGBA
+	glClearColor(0.53f, 0.81f, 0.92f, 1.0); //set sky blue clear color RGBA
 	int width, height; //window width and height
 	SDL_GetWindowSize(window, &width, &height); //get current window size
 	glViewport(0, 0, width, height); //create viewport
@@ -117,7 +109,27 @@ int init()
 	SDL_GL_SwapWindow(window); //swap buffers
 	glClear(GL_COLOR_BUFFER_BIT); //clear back buffer
 	glDisable(GL_CULL_FACE); //disable face culling for now
-	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); //draw wireframe for now
+	//create default shader
+	GLuint defaultVert = 0;
+	GLuint defaultFrag = 0;
+	defaultVert = glCreateShader(GL_VERTEX_SHADER); //initalize shader
+	defaultFrag = glCreateShader(GL_FRAGMENT_SHADER); //..
+	glShaderSource(defaultVert, 1, &vert_default, NULL); //load shader source
+	glCompileShader(defaultVert); //compile shader source
+	glShaderSource(defaultFrag, 1, &frag_default, NULL); //..
+	glCompileShader(defaultFrag); //..
+
+	errShader = glCreateProgram(); //initalize program
+	glAttachShader(errShader, defaultVert); //attach vert shader
+	glAttachShader(errShader, defaultFrag); //.. frag ..
+	glLinkProgram(errShader); //link shaders into one program
+
+	for (unsigned int i = 1; i <= MAX_SPRITES; i++)
+	{
+		FreeInstancedAttributeIndices.push(MAX_SPRITES - i); //populate free indices stack
+	}
+
 	//Box2d
 	b2WorldDef worldDef = b2DefaultWorldDef(); //create a world definition for box2d
 	b2WorldId pWorld = b2CreateWorld(&worldDef); //create a box2d world from that definition
@@ -156,11 +168,15 @@ static void handleKeys(SDL_KeyboardEvent* key)
 void loop()
 {
 	handleEvents();
-	SDL_GL_SwapWindow(window);
-
-
 }
 
 void draw()
 {
+	//...
+	SDL_GL_SwapWindow(window); //swap buffers
+}
+
+static unsigned int GetNewInstancedAttributeIndex()
+{
+
 }
